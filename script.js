cf68621fa5067486861aa7b92d9badc253468cf4
lLuckyFauzi <luckyfauzi2004@gmail.com>
2022-02-04 08:52:20 +0700

    contoh

---
+++ script.js
@@ -0,0 +1,29 @@
+gsap.to("#bg1", {
+  scrollTrigger : {
+    scrub: 6
+  },
+  scale: 1.3
+})
+
+
+
+gsap.to("#bg2", {
+  scrollTrigger : {
+    scrub: 1
+  },
+  scale: 0.8
+})
+
+gsap.to("#text", {
+  scrollTrigger : {
+    scrub: 1
+  },
+  y: -400
+})
+
+gsap.to("#nav", {
+  scrollTrigger : {
+    scrub: 1
+  },
+  y: 100
+})
